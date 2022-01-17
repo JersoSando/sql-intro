f0a270c5f08d801af36a2355b3aa3aaa616c43f6
Jerso Sando <jerso.sando@gmail.com>
2022-01-17 16:30:03 -0700

    groupby commit

---
+++ group_by.sql
@@ -0,0 +1,15 @@
+-- 1
+SELECT SUM(total) 
+FROM invoice
+GROUP BY billing_state;
+
+-- 2
+SELECT AVG(milliseconds), album_id
+FROM track
+GROUP BY album_id;
+
+-- 3
+SELECT COUNT(*), album_id
+FROM album
+WHERE artist_id IN (8, 22)
+GROUP BY artist_id;
